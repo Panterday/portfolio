2d653632e393e152d0f01f146e33bf11d2ffde6a
David <chavezzday@gmail.com>
2021-06-01 13:28:41 -0500

    gisap working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import style from "./styles.css";
-import logo from "./assets/icon.ico"
-import pic1 from "./assets/webpack.png"
+import gsap from 'gsap'; 
+import './styles.css';
 
-console.log("hola prueba")
+console.log("hola prueba 2")
+
+gsap.from('#logo', {opacity: 0, duration: 1, y: -50})
